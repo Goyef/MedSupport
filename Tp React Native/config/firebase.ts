e0ee218ab149f96bb127a2cfdc02db2a06b1d2eb
Antoine <saruelucas2@gmail.com>
2025-02-20 10:07:08 +0100

    modification config firebase pour persister l'etat firebase

--- Tp React Native/config/firebase.ts
+++ Tp React Native/config/firebase.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth, getReactNativePersistence, initializeAuth } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,4 +22,9 @@ const firebaseConfig = {
  const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 // Initialize Firebase Authentication and get a reference to the service
+
+const auth1 = initializeAuth(app, {
+  persistence:getReactNativePersistence(AsyncStorage),
+})
+
 export const auth = getAuth(app);
--- Tp React Native/services/auth.service.ts
+++ Tp React Native/services/auth.service.ts
@@ -7,3 +7,4 @@ export const signin = (
 ):Promise<UserCredential> => {
     return signInWithEmailAndPassword(auth,email,password)
 }
+
